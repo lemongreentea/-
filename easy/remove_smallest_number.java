a99c6c49c738c965f74b02ec9fb54b40d7f7a1e9
lemon <skfhddl003@naver.com>
2024-08-27 12:37:34 +0900

    제일 작은 수 제거하기

--- easy/remove_smallest_number.java
+++ easy/remove_smallest_number.java
@@ -1,23 +1,19 @@
 import java.util.Arrays;
 
 public class remove_smallest_number {
-    public int[] solution(int[] arr) {
+    public static int[] solution(int[] arr) {
         int[] answer ;
 
         if(arr.length == 1) return new int[]{-1};
 
         answer = Arrays.stream(arr).filter(e-> e!=Arrays.stream(arr).min().getAsInt()).toArray();
 
-        for(int s : answer){
-            System.out.println(s);
-        }
         return answer;
     }
 
     public static void main(String[] args) {
-        Test3 t2 = new Test3();
         // int[] numbers = {5,8,4,0,6,7,9};
         int[] numbers = {9};
-        System.out.println(t2.solution(numbers));
+        System.out.println(solution(numbers));
     }
 }
